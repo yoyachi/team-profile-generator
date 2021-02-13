a0d25da1645486555d82bfc2125c87f75dc59329
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-02-13 12:09:24 -0600

    cleaned code, added output team.html, ran test on classes/all passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,20 +2,20 @@ const Engineer = require("../lib/Engineer.js");
 
 test("Can ser GitHub account via constructor", () => {
     const testValue = "GitHubUser";
-    const e = new Engineer("Brandon", 1, "test@test.com, testValue", "Engineer");
+    const e = new Engineer("Brandon", 1, "test@test.com", "GitHubUser");
     expect (e.github).toBe(testValue);
 
 });
 
 test("getRole() should return \"Engineer\"", () => {
     const testValue = "Engineer";
-    const e = new Engineer("Brandon", 1, "test@test.com", "GitHubUser", "Engineer");
+    const e = new Engineer("Brandon", 1, "test@test.com", "Engineer");
     expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get GitHub username cia getGtihub()", () => {
+test("Can get GitHub username cia getGihub()", () => {
     const testValue = "GitHubUser";
-    const e = new Engineer("Bill", 1, "test@test.com", testValue, "Engineer");
+    const e = new Engineer("Bill", 1, "test@test.com", testValue);
     expect(e.getGithub()).toBe(testValue);
 });
 
